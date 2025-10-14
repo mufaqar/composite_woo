3885fcab37bd7d8098ab8d6586c5b3031a7cd423
Mufaqar <mufaqar@gmail.com>
2025-10-14 20:22:30 +0500

    Blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,50 +4,7 @@ import BlogGrid from "@/components/Blogs/BlogGrid";
 import FeaturedIcons from "@/components/HomePage/FeaturedIcons";
 import client from "@/lib/apollo-client";
 import { GET_POSTS } from "@/lib/queries/getPosts";
-
-// --------------------
-// TypeScript Interfaces
-// --------------------
-
-// Generic GraphQL "maybe" helper
-type Maybe<T> = T | null | undefined;
-
-// Author
-interface Author {
-  node: {
-    name: string;
-  };
-}
-
-// Featured Image
-interface FeaturedImage {
-  node: {
-    sourceUrl: string;
-    altText?: string;
-  };
-}
-
-// Single Post Type
-export interface Post {
-  id: string;
-  title: string;
-  slug: string;
-  excerpt: string;
-  date: string;
-  featuredImage?: FeaturedImage;
-  author?: Author;
-}
-
-// Root Query Type
-export interface GetPostsQuery {
-  posts?: {
-    nodes?: Maybe<Array<Maybe<Post>>>;
-  };
-}
-
-// --------------------
-// Page Component
-// --------------------
+import { GetPostsQuery, Post } from "@/lib/gql-types";
 
 export default async function BlogPage() {
   const { data } = await client.query<GetPostsQuery>({
--- lib/gql-types.ts
+++ lib/gql-types.ts
@@ -1,5 +1,39 @@
-export type GetPostsQuery = {
+// --------------------
+// TypeScript Interfaces
+// --------------------
+
+// Generic GraphQL "maybe" helper
+type Maybe<T> = T | null | undefined;
+
+// Author
+interface Author {
+  node: {
+    name: string;
+  };
+}
+
+// Featured Image
+interface FeaturedImage {
+  node: {
+    sourceUrl: string;
+    altText?: string;
+  };
+}
+
+// Single Post Type
+export interface Post {
+  id: string;
+  title: string;
+  slug: string;
+  excerpt: string;
+  date: string;
+  featuredImage?: FeaturedImage;
+  author?: Author;
+}
+
+// Root Query Type
+export interface GetPostsQuery {
   posts?: {
     nodes?: Maybe<Array<Maybe<Post>>>;
   };
-};
+}
