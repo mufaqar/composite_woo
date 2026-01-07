fe9e456377acc17131aca267ef3f782605936102
Mufaqar <mufaqar@gmail.com>
2026-01-07 19:29:34 +0500

    Seller

--- components/Product/ProductBox.tsx
+++ components/Product/ProductBox.tsx
@@ -4,10 +4,13 @@ import React from "react";
 import { CiHeart } from "react-icons/ci";
 
 const ProductBox = ({ data }: any) => {
- console.log("Product Box",data);
+  //console.log("Product Box",data);
 
   const regularPrice = data.regular_price || data.price;
   const salePrice = data.sale_price;
+
+  const badges = data.acf.product_badges || [];
+  console.log("Badges:", badges);
   return (
     <div className="bg-background">
       <div className="group ">
@@ -44,88 +47,99 @@ const ProductBox = ({ data }: any) => {
           <span className="text-description ml-1 ">{data.rating}</span>
         </div>
 
-       <div className="px-4">
-        <Link
-          href={`/product/${data.slug}`}
-          className="text-lg font-bold mt-2 text-black "
-        >
-          {data.name}
-        </Link>
+        <div className="px-4">
+          <Link
+            href={`/product/${data.slug}`}
+            className="text-lg font-bold mt-2 text-black "
+          >
+            {data.name}
+          </Link>
         </div>
       </div>
       <div className="mt-4 px-4">
         {/* Icons row */}
         <div className="flex gap-3 mt-3">
-          <button className="flex flex-col justify-center items-center group relative w-fit">
-            <span className="bg-primary min-w-[81px] w-full text-white text-xs font-normal font-Satoshi px-1.5 py-0.5 rounded translate-y-8 transition-all ease-in-out duration-300 hidden group-hover:block absolute group-hover:-translate-y-10">
-              fast Delivery
-            </span>
-            <span className="w-13 h-13 bg-[#00DFA21C] rounded-full flex items-center justify-center text-primary p-2">
-              <Image
-                src="/images/products-icon-4.svg"
-                width={500}
-                height={500}
-                alt="Picture"
-              />
-            </span>
-          </button>
-          <button className="flex flex-col justify-center items-center group relative w-fit">
-            <span className="bg-primary min-w-[81px] w-full text-white text-xs font-normal font-Satoshi px-1.5 py-0.5 rounded translate-y-8 transition-all ease-in-out duration-300 hidden group-hover:block absolute group-hover:-translate-y-10">
-              fast Delivery
-            </span>
-            <span className="w-13 h-13 bg-[#00DFA21C] rounded-full flex items-center justify-center text-primary p-2">
-              <Image
-                src="/images/products-icon-1.svg"
-                width={500}
-                height={500}
-                alt="Picture"
-              />
-            </span>
-          </button>
-          <button className="flex flex-col justify-center items-center group relative w-fit">
-            <span className="bg-primary min-w-[81px] w-full text-white text-xs font-normal font-Satoshi px-1.5 py-0.5 rounded translate-y-8 transition-all ease-in-out duration-300 hidden group-hover:block absolute group-hover:-translate-y-10">
-              fast Delivery
-            </span>
-            <span className="w-13 h-13 bg-[#00DFA21C] rounded-full flex items-center justify-center text-primary p-2">
-              <Image
-                src="/images/products-icon-3.svg"
-                width={500}
-                height={500}
-                alt="Picture"
-              />
-            </span>
-          </button>
-          <button className="flex flex-col justify-center items-center group relative w-fit">
-            <span className="bg-primary min-w-[81px] w-full text-white text-xs font-normal font-Satoshi px-1.5 py-0.5 rounded translate-y-8 transition-all ease-in-out duration-300 hidden group-hover:block absolute group-hover:-translate-y-10">
-              fast Delivery
-            </span>
-            <span className="w-13 h-13 bg-[#00DFA21C] rounded-full flex items-center justify-center text-primary p-2">
-              <Image
-                src="/images/products-icon-2.svg"
-                width={500}
-                height={500}
-                alt="Picture"
-              />
-            </span>
-          </button>
+          {badges?.best_seller && (
+            <button className="flex flex-col justify-center items-center group relative w-fit">
+              <span className="bg-primary min-w-[81px] w-full text-white text-xs font-normal font-Satoshi px-1.5 py-0.5 rounded translate-y-8 transition-all ease-in-out duration-300 hidden group-hover:block absolute group-hover:-translate-y-10">
+                {badges.best_seller}
+              </span>
+
+              <span className="w-13 h-13 bg-[#00DFA21C] rounded-full flex items-center justify-center text-primary p-2">
+                <Image
+                  src="/images/products-icon-4.svg"
+                  width={500}
+                  height={500}
+                  alt="Fast Delivery"
+                />
+              </span>
+            </button>
+          )}
+
+          {badges?.quality_assured && (
+            <button className="flex flex-col justify-center items-center group relative w-fit">
+              <span className="bg-primary min-w-[81px] w-full text-white text-xs font-normal font-Satoshi px-1.5 py-0.5 rounded translate-y-8 transition-all ease-in-out duration-300 hidden group-hover:block absolute group-hover:-translate-y-10">
+                {badges.quality_assured}
+              </span>
+              <span className="w-13 h-13 bg-[#00DFA21C] rounded-full flex items-center justify-center text-primary p-2">
+                <Image
+                  src="/images/products-icon-1.svg"
+                  width={500}
+                  height={500}
+                  alt="Picture"
+                />
+              </span>
+            </button>
+          )}
+
+          {badges?.on_sale && (
+            <button className="flex flex-col justify-center items-center group relative w-fit">
+              <span className="bg-primary min-w-[81px] w-full text-white text-xs font-normal font-Satoshi px-1.5 py-0.5 rounded translate-y-8 transition-all ease-in-out duration-300 hidden group-hover:block absolute group-hover:-translate-y-10">
+                {badges.on_sale}
+              </span>
+              <span className="w-13 h-13 bg-[#00DFA21C] rounded-full flex items-center justify-center text-primary p-2">
+                <Image
+                  src="/images/products-icon-3.svg"
+                  width={500}
+                  height={500}
+                  alt="Picture"
+                />
+              </span>
+            </button>
+          )}
+          {badges?.delivery && (
+            <button className="flex flex-col justify-center items-center group relative w-fit">
+              <span className="bg-primary min-w-[81px] w-full text-white text-xs font-normal font-Satoshi px-1.5 py-0.5 rounded translate-y-8 transition-all ease-in-out duration-300 hidden group-hover:block absolute group-hover:-translate-y-10">
+                {badges.delivery}
+              </span>
+              <span className="w-13 h-13 bg-[#00DFA21C] rounded-full flex items-center justify-center text-primary p-2">
+                <Image
+                  src="/images/products-icon-2.svg"
+                  width={500}
+                  height={500}
+                  alt="Picture"
+                />
+              </span>
+            </button>
+          )}
         </div>
         {/* Price */}
-       <div className="mt-3 flex items-center gap-2">
-  {salePrice ? (
-    <>
-      <span className="text-description line-through">
-        £{regularPrice}
-      </span>
-      <span className="text-title font-semibold text-xl">
-        £{salePrice}
-      </span>
-    </>
-  ) : (
-    <span className="text-title font-semibold text-xl">
-      £{regularPrice}
-    </span>
-  )}
-</div>
+        <div className="mt-3 flex items-center gap-2">
+          {salePrice ? (
+            <>
+              <span className="text-description line-through">
+                £{regularPrice}
+              </span>
+              <span className="text-title font-semibold text-xl">
+                £{salePrice}
+              </span>
+            </>
+          ) : (
+            <span className="text-title font-semibold text-xl">
+              £{regularPrice}
+            </span>
+          )}
+        </div>
 
         <div className="flex md:flex-row flex-col items-center justify-center gap-3 py-4 ">
           <Link
